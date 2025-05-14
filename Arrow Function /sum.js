55b8488285e5abfb972fd94bcb59272c01658179
afzal khan <afzalkhan01765@gmail.com>
2025-05-14 08:35:56 +0600

    change code

--- Arrow Function /sum.js
+++ Arrow Function /sum.js
@@ -5,7 +5,11 @@ function sum (a,b){    // This is actullay normal function .
 
 sumwitharrow = (a,b)=>{    // This is actullay arrow function . 
     return a + b ;
-}  
+} 
+sumwitharrow2 = (a,b)=>a+b;  // with out return keyword . 
 
 console.log(sumwitharrow(23,12));
+
 console.log(sum(23,12));
+
+console.log(sumwitharrow2(12,33));
