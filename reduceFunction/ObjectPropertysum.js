45d696144a116bafbd9023a998446c3bca7a029f
afzal khan <afzalkhan01765@gmail.com>
2025-05-14 12:02:48 +0600

    file add

--- reduceFunction/ObjectPropertysum.js
+++ reduceFunction/ObjectPropertysum.js
@@ -1,7 +1,9 @@
 const items = [
+
   { name: 'pen', price: 10 },
   { name: 'book', price: 40 },
   { name: 'pencil', price: 5 }
+  
 ];
 
 const total = items.reduce((acc , item) => acc + item.price , 0);
