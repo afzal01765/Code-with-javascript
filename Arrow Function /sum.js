fb84eccdc9d4f77af9822e9925b5476b35a11e45
afzal khan <afzalkhan01765@gmail.com>
2025-05-14 08:50:54 +0600

    file add

--- Arrow Function /sum.js
+++ Arrow Function /sum.js
@@ -1,15 +1,25 @@
-function sum (a,b){    // This is actullay normal function . 
-    return a + b; 
+function sum (a,b){   // This is actullay normal function . 
+    return a + b;
 }
-
 
 sumwitharrow = (a,b)=>{    // This is actullay arrow function . 
     return a + b ;
-} 
-sumwitharrow2 = (a,b)=>a+b;  // with out return keyword . 
+}
+
+sumwitharrow2 = (a,b)=>a+b;  // without return keyword . 
+
+let hello =()=>"Hello World";  //defult 
+
+let Hello = ()=>"Hello world"
+
 
 console.log(sumwitharrow(23,12));
 
 console.log(sum(23,12));
 
 console.log(sumwitharrow2(12,33));
+
+console.log(hello());
+
+console.log(Hello())
+
